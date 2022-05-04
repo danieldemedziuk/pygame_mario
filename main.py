bf5708fbbf0551749a95329e14697f07bda9e2d9
danieldemedziuk <danieldemedziuk@gmail.com>
2022-05-04 19:35:05 +0200

    add drawing level

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import pygame, sys
+from settings import *
+from level import Level
 
 # Pygame setup
 pygame.init()
-screen_width = 1200
-screen_height = 700
 screen = pygame.display.set_mode((screen_width, screen_height))
 clock = pygame.time.Clock()
+level = Level(level_map, screen)
 
 while True:
     for event in pygame.event.get():
@@ -14,6 +15,7 @@ while True:
             sys.exit()
 
     screen.fill('black')
+    level.run()
 
     pygame.display.update()
     clock.tick(60)
