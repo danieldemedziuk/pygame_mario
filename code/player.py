1d9e04b520a1d0f872eff6ca835c1f3b8616b1a0
Daniel Demedziuk <daniel.demedziuk@gmail.com>
2022-05-10 21:11:15 +0200

    added image for player and simple animation

--- code/player.py
+++ code/player.py
@@ -6,8 +6,9 @@ class Player(pygame.sprite.Sprite):
     def __init__(self, pos):
         super().__init__()
         self.import_character_assets()
-        self.image = pygame.Surface((32, 64))
-        self.image.fill('red')
+        self.frame_index = 0
+        self.animation_speed = 0.15
+        self.image = self.animations['idle'][self.frame_index]
         self.rect = self.image.get_rect(topleft=pos)
 
         # player movement
@@ -28,6 +29,16 @@ class Player(pygame.sprite.Sprite):
         for animation in self.animations.keys():
             full_path = character_path + animation
             self.animations[animation] = import_folder(full_path)
+
+    def animate(self):
+        animation = self.animations['run']
+
+        # loop over frame index
+        self.frame_index += self.animation_speed
+        if self.frame_index >= len(animation):
+            self.frame_index = 0
+
+        self.image = animation[int(self.frame_index)]
 
     def get_input(self):
         keys = pygame.key.get_pressed()
@@ -51,4 +62,5 @@ class Player(pygame.sprite.Sprite):
 
     def update(self):
         self.get_input()
+        self.animate()
 
