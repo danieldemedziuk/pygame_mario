295081f50f499a714161a4453b5799e3d5ad34ba
Daniel Demedziuk <daniel.demedziuk@gmail.com>
2022-05-12 20:07:35 +0200

    repair direction in horizontal movement collision

--- code/level.py
+++ code/level.py
@@ -10,8 +10,8 @@ class Level:
         # level setup
         self.display_surface = surface
         self.setup_level(level_data)
-
         self.world_shift = 0
+        self.current_x = 0
 
     def setup_level(self, layout):
         self.tiles = pygame.sprite.Group()
@@ -52,8 +52,16 @@ class Level:
             if sprite.rect.colliderect(player.rect):
                 if player.direction.x < 0:
                     player.rect.left = sprite.rect.right
+                    player.on_left = True
+                    self.current_x = player.rect.left
                 elif player.direction.x > 0:
                     player.rect.right = sprite.rect.left
+                    player.on_right = True
+                    self.current_x = player.rect.right
+        if player.on_left and (player.rect.left < self.current_x or player.direction.x >= 0):
+            player.on_left = False
+        if player.on_right and (player.rect.right > self.current_x or player.direction.x <= 0):
+            player.on_right = False
 
     def vertical_movement_collision(self):
         player = self.player.sprite
@@ -69,6 +77,7 @@ class Level:
                     player.rect.top = sprite.rect.bottom
                     player.direction.y = 0
                     player.on_ceiling = True
+
         if player.on_ground and player.direction.y < 0 or player.direction.y > 1:
             player.on_ground = False
         if player.on_ceiling and player.direction.y > 0:
