60f789e01d2cc4c745d837eeb7434e41f00d541d
Daniel Demedziuk <daniel.demedziuk@gmail.com>
2022-05-12 19:47:37 +0200

    added animation for player - one side

--- code/player.py
+++ code/player.py
@@ -17,6 +17,9 @@ class Player(pygame.sprite.Sprite):
         self.gravity = 0.8
         self.jump_speed = -16
 
+        # player status
+        self.status = 'idle'
+
     def import_character_assets(self):
         character_path = '../graphics/character/'
         self.animations = {
@@ -31,7 +34,7 @@ class Player(pygame.sprite.Sprite):
             self.animations[animation] = import_folder(full_path)
 
     def animate(self):
-        animation = self.animations['run']
+        animation = self.animations[self.status]
 
         # loop over frame index
         self.frame_index += self.animation_speed
@@ -53,6 +56,17 @@ class Player(pygame.sprite.Sprite):
         if keys[pygame.K_SPACE]:
             self.jump()
 
+    def get_status(self):
+        if self.direction.y < 0:
+            self.status = 'jump'
+        elif self.direction.y > 1:
+            self.status = 'fall'
+        else:
+            if self.direction.x != 0:
+                self.status = 'run'
+            else:
+                self.status = 'idle'
+
     def apply_gravity(self):
         self.direction.y += self.gravity
         self.rect.y += self.direction.y
@@ -62,5 +76,6 @@ class Player(pygame.sprite.Sprite):
 
     def update(self):
         self.get_input()
+        self.get_status()
         self.animate()
 
