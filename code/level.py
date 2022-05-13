43a1332f318019f017449ec043c06ca1036b7ee6
danieldemedziuk <danieldemedziuk@gmail.com>
2022-05-13 22:15:57 +0200

    added jump animation

--- code/level.py
+++ code/level.py
@@ -2,6 +2,7 @@ import pygame
 from tiles import Tile
 from settings import *
 from player import Player
+from particles import ParticleEffect
 
 
 class Level:
@@ -12,6 +13,13 @@ class Level:
         self.setup_level(level_data)
         self.world_shift = 0
         self.current_x = 0
+
+        # dust
+        self.dust_sprite = pygame.sprite.GroupSingle()
+
+    def create_jump_particles(self, pos):
+        jump_particle_sprite = ParticleEffect(pos, 'jump')
+        self.dust_sprite.add(jump_particle_sprite)
 
     def setup_level(self, layout):
         self.tiles = pygame.sprite.Group()
@@ -26,7 +34,7 @@ class Level:
                     tile = Tile((x, y), tile_size)
                     self.tiles.add(tile)
                 if cell == 'P':
-                    player_sprite = Player((x, y), self.display_surface)
+                    player_sprite = Player((x, y), self.display_surface, self.create_jump_particles)
                     self.player.add(player_sprite)
 
     def scroll_x(self):
@@ -89,6 +97,10 @@ class Level:
         self.tiles.update(self.world_shift)
         self.tiles.draw(self.display_surface)
         self.scroll_x()
+
+        # dust particles
+        self.dust_sprite.update(self.world_shift)
+        self.dust_sprite.draw(self.display_surface)
 
         # player
         self.player.update()
