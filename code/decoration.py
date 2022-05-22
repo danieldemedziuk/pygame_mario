d496f5c104c7da0ae9854a963f5027a0444c24de
danieldemedziuk <danieldemedziuk@gmail.com>
2022-05-22 11:09:46 +0200

    added player - repair fg palms

--- code/decoration.py
+++ code/decoration.py
@@ -1,5 +1,5 @@
-import pygame
 from settings import vertical_tile_number, tile_size, screen_width
+import pygame
 from tiles import AnimatedTile, StaticTile
 from support import import_folder
 from random import choice, randint
@@ -32,7 +32,7 @@ class Water:
     def __init__(self, top, level_width):
         water_start = -screen_width
         water_tile_width = 192
-        tile_x_amount = int((level_width + screen_width) / water_tile_width)
+        tile_x_amount = int((level_width + screen_width * 2) / water_tile_width)
         self.water_sprites = pygame.sprite.Group()
 
         for tile in range(tile_x_amount):
